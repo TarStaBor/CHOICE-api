431dd28a24b7ed289b4217febb30c5beeb711360
TarStaBor <TarStaBor@yandex.ru>
2022-05-18 06:55:59 +0300

    fix: delete an unnecessary message "SuccessCommentUpdate"

--- utils/messages.js
+++ utils/messages.js
@@ -2,7 +2,6 @@ const errorMessages = {
   SuccessResponse: "Отклик получен",
   SuccessJobDelete: "Вакансия удалена",
   SuccessApplicantDelete: "Отклик удален",
-  SuccessCommentUpdate: "Комментарий успешно обновлен",
 };
 
 module.exports = errorMessages;
